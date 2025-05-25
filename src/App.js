1299c843e5a491ab5e699f5684c6598e516fd78c
khloud000 <khloudmo000@gmail.com>
2025-05-25 16:36:35 +0300

    Last

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Documentmanager from "./pages/Documentmanager"
+
+const App = () => {
+  return (
+    <Router>
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Documentmanager/>} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
