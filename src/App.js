94ecacf90b20312449b775aa50d9ff3bc0a30cef
Khloud-Mohamed-S <fork55516@gmail.com>
2025-05-25 17:40:10 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Documentmanager from "./pages/Documentmanager"
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Documentmanager from './pages/Documentmanager';
+
 const App = () => {
   return (
     <Router>
       <div className="p-4">
         <Routes>
           <Route path="/" element={<Documentmanager/>} />
-        <Routes>
+        </Routes>
       </div>
     </Router>
   );
